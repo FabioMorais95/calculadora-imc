7d44395476707063e7b2787d466a3e8d844c50c1
Fábio Morais <fabio.gunner@live.com>
2022-12-01 10:01:46 -0300

    versão mobile criada

---
+++ js.js
@@ -0,0 +1,34 @@
+
+const btn = document.querySelector('.calcular')
+
+
+btn.addEventListener('click', () => {
+    const nome = document.querySelector(".nome").value
+    const altura = document.querySelector('.altura').value
+    const peso = document.querySelector('.peso').value
+    const calculo = (peso / (altura * altura)).toFixed(1)
+    let classificacao = ""
+
+    if(nome == '' || altura =='' || peso == ''){
+        resultado = document.querySelector('#resultado').innerHTML = 'Preencha todos os campos'
+    }
+
+    if (calculo < 18) {
+        classificacao = 'Abaixo do peso'
+    } else if (calculo < 25) {
+        classificacao = 'Peso ideal'
+    }else if(calculo < 29.9){
+        classificacao = 'Sobrepeso'
+    }else if(calculo < 39){
+        classificacao = 'Obesidade'
+    }else if(calculo > 40){
+        classificacao = 'Obesidade grave'
+    }
+    const resultado = document.querySelector('#resultado').innerHTML = `${nome}, Seu imc é: ${calculo} e você está ${classificacao}`
+    clearInputs()
+})
+function clearInputs(){
+    document.querySelector(".nome").value = ''
+    document.querySelector('.altura').value = ''
+    document.querySelector('.peso').value = ''
+}
